7057d0188919d9a10b3dd1f425ce94c5a38fcecd
eduardolujan <infinito15A>
2018-11-26 22:52:23 -0600

    2nd Commit, better graphic interface and sqlite database

--- app/src/main/java/com/example/android/mandown/Main2Activity.kt
+++ app/src/main/java/com/example/android/mandown/Main2Activity.kt
@@ -1,6 +1,8 @@
 package com.example.android.mandown
 
 import android.arch.persistence.room.Room
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -15,16 +17,20 @@ import kotlinx.android.synthetic.main.activity_main2.*
 import kotlinx.android.synthetic.main.app_bar_main2.*
 import android.support.v4.content.ContextCompat.startActivity
 import android.content.Intent
+import android.os.Build
+import android.support.annotation.RequiresApi
 import android.support.v4.view.accessibility.AccessibilityEventCompat.setAction
+import android.telephony.SmsMessage
 import android.util.Log
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import com.example.android.mandown.R.id.*
 import kotlinx.android.synthetic.main.content_main2.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
 
-class Main2Activity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, AdapterView.OnItemSelectedListener  {
+class Main2Activity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, AdapterView.OnItemSelectedListener {
     override fun onNothingSelected(parent: AdapterView<*>?) {
         }
 
@@ -47,8 +53,8 @@ class Main2Activity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
             //        .setAction("Action", null).show()
 
             //  Toast.makeText(this, "holi", Toast.LENGTH_SHORT).show()
-            val myIntent = Intent(this, ManDown::class.java)
-            // myIntent.putExtra("key", value) //Optional parameters
+            val myIntent = Intent(this, ManDown2::class.java)
+             myIntent.putExtra("area", spinner!!.selectedItem.toString()) //Optional parameters
             startActivity(myIntent)
         }
 
@@ -111,8 +117,72 @@ class Main2Activity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
 
 
     fun populateData(): Array<TaskEntity> {
-        return arrayOf<TaskEntity>(TaskEntity(1, "Area 1", "1234"),
-                TaskEntity(2, "Area 2", "00000"), TaskEntity(3, "Area 3", "5555"))
+        return arrayOf<TaskEntity>(
+                TaskEntity(1, "BO Cuarto de resinas norte", "1234"),
+                TaskEntity(2, "BO Cuarto de resinas sur", "1234"),
+                TaskEntity(3, "BO Trinchera Norte", "1234"),
+                TaskEntity(4, "BO Trinchera Sur", "1234"),
+                TaskEntity(5, "BO Cuarto de Utilities BO Norte", "1234"),
+                TaskEntity(6, "BO Cuarto de Utilities BO Sur", "1234"),
+                TaskEntity(7, "BO Mezzanine Norte (Bombas de resinas)", "1234"),
+                TaskEntity(8, "BO Mezzanine Sur (Bombas de resinas)", "1234"),
+                TaskEntity(9, "BO Subestaciòn Eléctrica BO 2", "1234"),
+                TaskEntity(10, "BO Subestación Eléctrica BO 1", "1234"),
+                TaskEntity(11, "BO Bodega de limpieza BO", "1234"),
+                TaskEntity(12, "BO Bodega de mantenimiento", "1234"),
+                TaskEntity(13, "BO Cuarto Generador de Vapor", "1234"),
+                TaskEntity(14, "BO Techumbre de BO ", "1234"),
+                TaskEntity(15, "BO BEDATEC (Hojas) ", "1234"),
+                TaskEntity(16, "BO P.K.S (Hojas)", "1234"),
+                TaskEntity(17, "BO Cuarto de Servicios de Blades", "1234"),
+                TaskEntity(18, "BO Cuarto de residuos orgánicos BO", "1234"),
+                TaskEntity(19, "BO Cuarto de IT BO", "1234"),
+                TaskEntity(20, "Exterior Planta de emergencia de BO", "1234"),
+                TaskEntity(21, "Exterior Camino periférico de planta", "1234"),
+                TaskEntity(22, "Exterior Parte superior Silos Sur", "1234"),
+                TaskEntity(23, "Exterior Parte superior Silos Norte", "1234"),
+                TaskEntity(24, "Exterior Torres de enfriamiento", "1234"),
+                TaskEntity(25, "Exterior Cuarto de bombeo", "1234"),
+                TaskEntity(26, "Exterior Pozo", "1234"),
+                TaskEntity(27, "Exterior Planta de Agua Potable", "1234"),
+                TaskEntity(28, "Exterior Planta de Tratamiento de aguas residuales", "1234"),
+                TaskEntity(29, "Exterior Área de tanques de distribución de agua del sistema de contra incendio PC", "1234"),
+                TaskEntity(30, "Exterior Cuarto de Bombas 1 FP", "1234"),
+                TaskEntity(31, "Exterior Cuarto de Bombas 2 FP", "1234"),
+                TaskEntity(32, "Exterior Canal Norte para agua pluvial", "1234"),
+                TaskEntity(33, "Exterior Canal Sur para agua pluvial", "1234"),
+                TaskEntity(34, "Exterior Tanque de Hidrógeno", "1234"),
+                TaskEntity(35, "Exterior Tanque de Nitrógeno", "1234"),
+                TaskEntity(36, "Exterior Cuarto de Residuos Peligrosos", "1234"),
+                TaskEntity(37, "Exterior Cuarto de Materiales Peligrosos", "1234"),
+                TaskEntity(38, "Exterior Cuarto de Obsoletos", "1234"),
+                TaskEntity(39, "Exterior Subestación Principal ", "1234"),
+                TaskEntity(40, "Exterior Estación de Gas LP ", "1234"),
+                TaskEntity(41, "Exterior Bomba 1 de descarga de Diesel", "1234"),
+                TaskEntity(42, "Exterior Bomba 2 de descarga de Diesel", "1234"),
+                TaskEntity(43, "Exterior Área de tanques pulmón BO-PC", "1234"),
+                TaskEntity(44, "Exterior Cuarto de lubricantes", "1234"),
+                TaskEntity(45, "Exterior Bodega de Control de plagas", "1234"),
+                TaskEntity(46, "Exterior Laguna de colección de agua pluvial", "1234"),
+                TaskEntity(47, "Exterior Bodega de Jardineria", "1234"),
+                TaskEntity(48, "Exterior Subestación eléctrica de torres de enfriamiento", "1234"),
+                TaskEntity(49, "Exterior Antena de TELCEL ", "1234"),
+                TaskEntity(50, "Exterior Sala de capacitación de contratistas", "1234"),
+                TaskEntity(51, "Exterior Evaporadora", "1234"),
+                TaskEntity(52, "Exterior Área de residuos (exterior)", "1234"),
+                TaskEntity(53, "PC Bodega de Limpieza PC ", "1234"),
+                TaskEntity(54, "PC Cuarto de Utilities PC", "1234"),
+                TaskEntity(55, "PC Techumbre PC ", "1234"),
+                TaskEntity(56, "PC Cuarto de residuos orgánicos PC", "1234"),
+                TaskEntity(57, "PC Cuarto de baterias ", "1234"),
+                TaskEntity(58, "PC Cuarto de IT PC ", "1234"),
+                TaskEntity(59, "PC Planta de emergencia de PC ", "1234"),
+                TaskEntity(60, "PC Laboratorio de Pack Dev ", "1234"),
+                TaskEntity(61, "PC Área de residuos (interior) ", "1234")
+
+
+
+        )
     }
     override fun onBackPressed() {
         if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
--- app/src/main/java/com/example/android/mandown/ManDown.kt
+++ app/src/main/java/com/example/android/mandown/ManDown.kt
@@ -1,5 +1,12 @@
 package com.example.android.mandown
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -13,36 +20,48 @@ import kotlinx.android.synthetic.main.app_bar_man_down.*
 import android.os.CountDownTimer
 import android.util.Log
 import android.widget.ProgressBar
+import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.android.synthetic.main.content_man_down.*
+import kotlin.math.sqrt
 
 
-class ManDown : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+//First Screen With Area Selection
+
+class ManDown : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, SensorEventListener {
     lateinit var mCountDownTimer: CountDownTimer
+    private lateinit var sensorManager: SensorManager
+    private lateinit var accelerometer: Sensor
+    private lateinit var gravity: Sensor
+    private lateinit var gyroscope: Sensor
+    private lateinit var linearAcceleration: Sensor
+    private lateinit var rotationVector: Sensor
     var i = 30
-
+    private val PERMISSION_REQUEST_CODE = 1
+    private var accelGravity = floatArrayOf(0.0f,0.0f,0.0f)
+    private var accelLin = floatArrayOf(0.0f,0.0f,0.0f)
+    private var  mAccel = 0.0f ;
+    private var  mAccelCurrent= 0.0f ;
+    private var  mAccelLast= 0.0f ;
+    private var messageSent: Boolean = false
+    private var gravityDetectionLevel: Int = 5
+    private var motionCounter: Int = 0
+    private var isInMovement: Boolean = true
+    private var timer: Long = 30000
+    private var mLight: Sensor? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_man_down)
         setSupportActionBar(toolbar)
 
+        mAccel = 0.00f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
+        //Call the start sensor Method
+       // lblSensibility.text = gravityDetectionLevel.toString()
+        startingSensor()
 
         mProgressBar.setProgress(i)
-        mCountDownTimer = object : CountDownTimer(30000, 1000) {
 
-            override fun onTick(millisUntilFinished: Long) {
-                Log.v("Log_tag", "Tick of Progress$i$millisUntilFinished")
-                i--
-                mProgressBar.setProgress(i * 100 / (30000 / 1000))
-
-            }
-
-            override fun onFinish() {
-                //Do what you want
-                i--
-                mProgressBar.setProgress(0)
-            }
-        }
-        mCountDownTimer.start()
 
 
         fab.setOnClickListener { view ->
@@ -56,7 +75,122 @@ class ManDown : AppCompatActivity(), NavigationView.OnNavigationItemSelectedList
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+
+            if (checkSelfPermission(Manifest.permission.SEND_SMS)
+                    == PackageManager.PERMISSION_DENIED) {
+
+                Log.d("permission", "permission denied to SEND_SMS - requesting it");
+                val permissions = arrayOf(Manifest.permission.SEND_SMS)
+                requestPermissions(permissions, PERMISSION_REQUEST_CODE)
+
+            }
+
+        }
+        mCountDownTimer = object : CountDownTimer(30000, 1000) {
+
+
+            override fun onTick(millisUntilFinished: Long) {
+                Log.d("Log_tag", "Tick of Progress$i$millisUntilFinished")
+                i--
+                mProgressBar.setProgress(i * 100 / (30000 / 1000))
+
+            }
+
+            override fun onFinish() {
+                //Do what you want
+                i--
+                mProgressBar.setProgress(0)
+            }
+        }
     }
+    //Starting the sensors
+    fun startingSensor(){
+        Log.d("Sensor","Starting")
+        this.sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.let {
+            this.accelerometer = it
+            Log.d("Sensor","Started Accelerometer")
+        }
+
+        sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)?.let {
+            this.gravity = it
+        }
+
+        sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)?.let {
+            this.gyroscope = it
+        }
+
+        sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)?.let {
+            this.linearAcceleration = it
+        }
+
+        sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)?.let {
+            this.rotationVector = it
+        }
+        // Get the default sensor of specified type
+        mLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+    }
+
+    //Sensor Events Listener
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        Log.d("MY_APP", event.toString())
+        Log.d("Sensor","Sensor Changed")
+        Log.d("Sensor",event?.sensor?.type.toString())
+
+        when (event?.sensor?.type) {
+
+
+            Sensor.TYPE_ACCELEROMETER -> {
+                val alpha = 0.8f
+                Log.d("MainActivity","Acelerometro")
+                accelGravity[0] = alpha * accelGravity[0] + (1 - alpha) * event.values[0]
+                accelGravity[1] = alpha * accelGravity[1] + (1 - alpha) * event.values[1]
+                accelGravity[2] = alpha * accelGravity[2] + (1 - alpha) * event.values[2]
+
+                accelLin[0] = event.values[0] - accelGravity[0]
+                accelLin[1] = event.values[1] - accelGravity[1]
+                accelLin[2] = event.values[2] - accelGravity[2]
+
+                tvAccelerometerX.text = accelLin[0].toString()
+                tvAccelerometerY.text = accelLin[1].toString()
+                tvAccelerometerZ.text = accelLin[2].toString()
+
+                val x = accelGravity[0]
+                val y = accelGravity[1]
+                val z = accelGravity[2]
+                mAccelLast = mAccelCurrent
+                mAccelCurrent = sqrt((x * x + y * y + z * z))
+                val delta = mAccelCurrent - mAccelLast
+                mAccel = mAccel * 0.9f + delta
+                // Make this higher or lower according to how much
+                // motion you want to detect
+                if (mAccel > gravityDetectionLevel) {
+                    // do something
+                    Log.d("Accelerometer","Is in movement")
+                    tvAccelerometerMovement.text = "Mestoymoviendo"
+                    motionCounter ++
+                    lblMotionCounter.text = motionCounter.toString()
+                    isInMovement = true
+                   // mCountDownTimer.cancel()
+                   // mCountDownTimer.start()
+
+                    //  val mp = MediaPlayer.create (this, R.raw.heylisten)
+                    //  mp.start ()
+                }
+                else{
+                    tvAccelerometerMovement.text = "Toyquieto"
+                }
+            }
+        }
+    }
+
 
     override fun onBackPressed() {
         if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
@@ -108,4 +242,15 @@ class ManDown : AppCompatActivity(), NavigationView.OnNavigationItemSelectedList
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
     }
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+    }
+
 }
